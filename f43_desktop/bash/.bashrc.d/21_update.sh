cc53dbe56c31c1c86c5d48c9c792f001b2211cf5
yz-5555 <yz5555@proton.me>
2026-05-10 02:31:18 +0900

    [feat] add update script for zls

--- f43_desktop/bash/.bashrc.d/21_update.sh
+++ f43_desktop/bash/.bashrc.d/21_update.sh
@@ -1,6 +1,16 @@
 function print_green() {
 	echo -e "\e[32m$1\e[0m"
 }
+function update-zls() {
+	local root="$HOME/Tools/zls"
+
+	git -C "$root" pull
+	zig build --build-file "$root/build.zig" -Doptimize=ReleaseSafe
+
+	if [ $? -eq 0 ]; then
+		print_green "'zls' was updated successfully!"
+	fi
+}
 function update-gfm() {
 	local root="$HOME/Documents/projects/git_fetch_mtime"
 
@@ -22,6 +32,7 @@ function update-ddf() {
 	fi
 }
 function update-all() {
+	# update-zls # commented because it should be synced with zig.
 	update-gfm
 	update-ddf
 
