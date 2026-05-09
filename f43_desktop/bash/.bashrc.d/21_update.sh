c6ae56a3fed2c900ebee21775dca54ec4c4255d7
yz-5555 <yz5555@proton.me>
2026-05-10 01:22:17 +0900

    20260509_f43

---
+++ f43_desktop/bash/.bashrc.d/21_update.sh
@@ -0,0 +1,33 @@
+function print_green() {
+	echo -e "\e[32m$1\e[0m"
+}
+function update-gfm() {
+	local root="$HOME/Documents/projects/git_fetch_mtime"
+
+	git -C "$root" pull
+	clang "$root/main.c" -o "$root/git_fetch_mtime" -Wall -Wextra -Wpedantic -std=c99 -O2
+
+	if [ $? -eq 0 ]; then
+		print_green "'git_fetch_mtime' was updated successfully!"
+	fi
+}
+function update-ddf() {
+	local root="$HOME/Documents/projects/ddf"
+
+	git -C "$root" pull
+	cargo build --manifest-path "$root/Cargo.toml" --release
+
+	if [ $? -eq 0 ]; then
+		print_green "'ddf' was updated successfully!"
+	fi
+}
+function update-all() {
+	update-gfm
+	update-ddf
+
+	pkill -f "syncthing" 2>/dev/null
+
+	sudo dnf upgrade --refresh
+
+	syncthing --no-browser >/dev/null 2>&1 &
+}
