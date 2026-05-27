73226765e84a4419c49c4df07d8f493300220dca
yz <yz025@proton.me>
2026-05-28 00:25:30 +0900

    20260528

--- fws/bash/.bashrc.d/00_env.sh
+++ fws/bash/.bashrc.d/00_env.sh
@@ -8,8 +8,6 @@ append_path "$HOME/Documents/projects/tocp/bin"
 append_path "$HOME/Documents/projects/git_fetch_mtime" # git_fetch_mtime
 
 export EDITOR="nvim"
-export TOCP_CONFIG_HOME="$HOME/.config"
-export DDF_TARGET="$HOME/Documents/dotfiles/"
 export SSH_AUTH_SOCK="$XDG_RUNTIME_DIR/ssh-agent.socket"
 
 # ssh agent
--- fws/bash/.bashrc.d/21_update.sh
+++ fws/bash/.bashrc.d/21_update.sh
@@ -7,12 +7,6 @@ function update-gfm() {
 	git -C "$root" pull
 	clang "$root/main.c" -o "$root/git_fetch_mtime" -Wall -Wextra -Wpedantic -std=c99 -O2
 }
-function update-ddf() {
-	local root="$HOME/Documents/projects/ddf"
-
-	git -C "$root" pull
-	cargo build --manifest-path "$root/Cargo.toml" --release
-}
 function update-all() {
 	sudo dnf upgrade --refresh
 	if [ $? -eq 0 ]; then
@@ -27,10 +21,5 @@ function update-all() {
     update-gfm
 	if [ $? -eq 0 ]; then
 		print_green "'git_fetch_mtime' was updated successfully!"
-	fi
-
-    update-ddf
-	if [ $? -eq 0 ]; then
-		print_green "'ddf' was updated successfully!"
 	fi
 }
