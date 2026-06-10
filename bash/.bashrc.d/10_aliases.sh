5a586a4e137a0a47795e226915f1074c88d9bf46
yz <yz025@proton.me>
2026-06-10 20:32:11 +0900

    [fix] wrong path

--- bash/.bashrc.d/10_aliases.sh
+++
@@ -1,3 +0,0 @@
-alias vim=nvim
-alias reload="source $HOME/.bashrc"
-alias adc=arduino-cli
--- nvim/after/ftplugin/rust.lua
+++
@@ -1,7 +0,0 @@
-local bufnr = vim.api.nvim_get_current_buf()
-vim.keymap.set('n', '<Leader>la', function()
-	vim.cmd.RustLsp('codeAction')
-end, { silent = true, buffer = bufnr })
-vim.keymap.set('n', '<Leader>lh', function()
-	vim.cmd.RustLsp({ 'hover', 'actions' })
-end, { silent = true, buffer = bufnr })
